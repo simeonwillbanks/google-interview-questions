268c05dc4f0b3e63eca74c644bf8efa4aa63605f
Simeon F. Willbanks <sfw@simeonfosterwillbanks.com>
2012-02-03 12:21:45 -0800

    Add files for question 12435684

---
+++ 12435684.rb
@@ -0,0 +1,6 @@
+# http://www.careercup.com/question?id=12435684
+# Generate all the possible substrings of a given string. Write code. 
+# i/p: abc
+# o/p: { a,b,c,ab,ac,bc,abc}
+def all_substrings(str)
+end
---
+++ test/12435684_test.rb
@@ -0,0 +1,6 @@
+require 'minitest/autorun'
+require_relative '../12435684.rb'
+
+class TestAllSubstrings < MiniTest::Unit::TestCase
+
+end
