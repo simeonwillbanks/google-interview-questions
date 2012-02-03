127c574c1f8b792475ca43b35788859b1b3c5729
Simeon F. Willbanks <sfw@simeonfosterwillbanks.com>
2012-02-03 13:47:38 -0800

    Add question 12478687 written by thor :new 12478687 -d

---
+++ questions/12478687.rb
@@ -0,0 +1,14 @@
+# http://www.careercup.com/question?id=12478687
+
+=begin
+First greater number in an array. Given a large array of positive
+integers, for an arbitrary integer A, we want to know the first integer in
+the array which is greater than or equal A . O(logn) solution required
+ex [2, 10,5,6,80]
+input : 6 output : 10
+input :20 output : 80
+=end
+
+def method_12478687
+
+end
---
+++ test/12478687_test.rb
@@ -0,0 +1,6 @@
+require 'minitest/autorun'
+require_relative '../12478687.rb'
+
+class TestMethod12478687 < MiniTest::Unit::TestCase
+
+end
