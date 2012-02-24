6ea088a80441652ad0e4e61ebc1a2378d96debd0
Simeon F. Willbanks <sfw@simeonfosterwillbanks.com>
2012-02-24 08:49:58 -0800

    Use bitwise operators to answer question

--- test/12590665_test.rb
+++ test/12590665_test.rb
@@ -3,4 +3,8 @@ require_relative '../questions/12590665.rb'
 
 class TestMethod12590665 < MiniTest::Unit::TestCase
 
+  def test_method_12590665
+    assert_equal 12, method_12590665(8, 3)
+  end
+
 end
