4d209dc27ecd63498e386e645fb6c2fda79d3024
Simeon F. Willbanks <sfw@simeonfosterwillbanks.com>
2012-02-28 08:11:09 -0800

    Implement two more solutions to two 12478687 including a divide-and-counquer algorithm and one which iterates the list only once

--- test/12590665_test.rb
+++ test/12590665_test.rb
@@ -4,7 +4,7 @@ require_relative '../questions/12590665.rb'
 class TestMethod12590665 < MiniTest::Unit::TestCase
 
   def test_method_12590665
-    assert_equal 12, method_12590665(8, 3)
+    assert_equal method_12590665(8, 3), 12
   end
 
 end
